2eaf14a0e65ab7f4f3990baf1ec92699e1a2d666
Thái Viết Phát <m0tnamk09@gmail.com>
2026-05-30 19:29:44 +0700

    update

---
+++ spring-security/src/main/java/phat/springframework/springsecurity/controllers/WelcomeController.java
@@ -0,0 +1,12 @@
+package phat.springframework.springsecurity.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to Spring Security Controller";
+    }
+}
